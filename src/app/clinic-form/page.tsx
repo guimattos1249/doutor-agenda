ca3c00c95b9e02bafd5a533048fd2938cf3eebd8
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-05-28 13:16:39 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,27 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = () => {
+  return (
+    <Dialog open={true}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar um clínica</DialogTitle>
+          <DialogDescription>
+            Adicione uma clínica para começar a usar o sistema.
+          </DialogDescription>
+        </DialogHeader>
+        <ClinicForm />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ClinicFormPage;
