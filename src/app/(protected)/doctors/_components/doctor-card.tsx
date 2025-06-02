e7239f78bb0e55a84966bf0631ab4e3c8fa29b91
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-02 16:44:02 -0300

    feat: ensure doctors are listed on doctors page

---
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { CalendarIcon, ClockIcon, DollarSign } from "lucide-react";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import { doctorsTable } from "@/db/schema";
+import { formatCurrencyInCents } from "@/helpers/currency";
+
+import { getAvailability } from "../_helpers/availability";
+import UpsertDoctorForm from "./upsert-doctor-form";
+
+interface DoctorCardProps {
+  doctor: typeof doctorsTable.$inferSelect;
+}
+
+const DoctorCard = ({ doctor }: DoctorCardProps) => {
+  const doctorInitials = doctor.name
+    .split(" ")
+    .map((name) => name[0])
+    .join("");
+
+  const availability = getAvailability(doctor);
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center gap-2">
+          <Avatar className="h-10 w-10">
+            <AvatarFallback>{doctorInitials}</AvatarFallback>
+          </Avatar>
+          <CardTitle>Médico</CardTitle>
+        </div>
+        <div>
+          <h3 className="text-sm font-medium">{doctor.name}</h3>
+          <p className="text-muted-foreground text-sm">{doctor.specialty}</p>
+        </div>
+      </CardHeader>
+      <Separator />
+      <CardContent className="flex flex-col gap-2">
+        <Badge variant="outline">
+          <CalendarIcon className="mr-1" />
+          {availability.from.format("dddd")} a {availability.to.format("dddd")}
+        </Badge>
+        <Badge variant="outline">
+          <ClockIcon className="mr-1" />
+          {availability.from.format("HH:mm")} -{" "}
+          {availability.to.format("HH:mm")}
+        </Badge>
+        <Badge variant="outline">
+          <DollarSign className="mr-1" />
+          {formatCurrencyInCents(doctor.appointmentPriceInCent)}
+        </Badge>
+      </CardContent>
+      <Separator />
+      <CardFooter>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="w-full">Ver detalhes</Button>
+          </DialogTrigger>
+          <UpsertDoctorForm />
+        </Dialog>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default DoctorCard;
